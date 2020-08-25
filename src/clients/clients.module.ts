98619c270df433de0ad08a3ffec5d5b3dcb66926
Aleksey Kotylev <aleks_rus@hotmail.com>
2020-08-25 18:42:56 -0300

    client module changes

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { clientSchema } from './schemas/client.schema';
+import { clientSchema, Client } from './schemas/client.schema';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './services/clients.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Client', schema: clientSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Client.name, schema: clientSchema }])],
   controllers: [ClientsController],
   providers: [ClientsService],
 })
